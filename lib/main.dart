68614172c7e1e9762b47fe870c4c107281e7af8a
malikjee760 <85275592+malikjee760@users.noreply.github.com>
2021-08-21 10:21:00 +0500

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,121 @@
-import 'dart:convert';
+
 //import 'dart:js';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
-void main() async {
-  // List mydata = await _getfetchdata_http();
-  List mydata = await getJsonData();
+import 'del_update.dart';
 
-  //print(mydata);
+// void main() async {
+//   // List mydata = await _getfetchdata_http();
+//   List mydata = await getJsonData();
+
+//   //print(mydata);
   
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Http Api'),
-        ),
+//   runApp(
+//     MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Http Api'),
+//         ),
          
-        body: GridView.count(
-  // Create a grid with 2 columns. If you change the scrollDirection to
-  // horizontal, this produces 2 rows.
-  crossAxisCount: 2,
-  // Generate 100 widgets that display their index in the List.
-  children: List.generate(mydata.length, (index) {
-    return Image.network(mydata[index]['url']);
-  }),
-)
-      ),
-    ),
-  );
-}
+//         body: GridView.count(
+//   // Create a grid with 2 columns. If you change the scrollDirection to
+//   // horizontal, this produces 2 rows.
+//   crossAxisCount: 2,
+//   // Generate 100 widgets that display their index in the List.
+//   children: List.generate(mydata.length, (index) {
+//     return Image.network(mydata[index]['url']);
+//   }),
+// )
+//       ),
+//     ),
+//   );
+// }
 
-// ignore: non_constant_identifier_names
-// Future<List> _getfetchdata_http() async {
-//   String url = 'https://jsonplaceholder.typicode.com/posts';
+// // ignore: non_constant_identifier_names
+// // Future<List> _getfetchdata_http() async {
+// //   String url = 'https://jsonplaceholder.typicode.com/posts';
+
+// //   http.Response response = await http.get(Uri.parse(url));
+
+// //   return jsonDecode(response.body);
+// // }
+
+// Future<List> getJsonData() async {
+//   String url = 'https://jsonplaceholder.typicode.com/photos';
 
 //   http.Response response = await http.get(Uri.parse(url));
 
 //   return jsonDecode(response.body);
 // }
 
-Future<List> getJsonData() async {
-  String url = 'https://jsonplaceholder.typicode.com/photos';
+void main() {
+  runApp(const MyApp());
+}
 
-  http.Response response = await http.get(Uri.parse(url));
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  return jsonDecode(response.body);
+  @override
+  _MyAppState createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  late Future<Album> _futureAlbum;
+
+  @override
+  void initState() {
+    super.initState();
+    _futureAlbum = fetchAlbum();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Delete Data Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Delete Data Example'),
+        ),
+        body: Center(
+          child: FutureBuilder<Album>(
+            future: _futureAlbum,
+            builder: (context, snapshot) {
+              // If the connection is done,
+              // check for response data or an error.
+              if (snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.hasData) {
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(snapshot.data?.title ?? 'Deleted'),
+                      ElevatedButton(
+                        child: const Text('Delete Data'),
+                        onPressed: () {
+                          setState(() {
+                            _futureAlbum =
+                                deleteAlbum(snapshot.data!.id.toString());
+                          });
+                        },
+                      ),
+                    ],
+                  );
+                } else if (snapshot.hasError) {
+                  return Text("${snapshot.error}");
+                }
+              }
+
+              // By default, show a loading spinner.
+              return const CircularProgressIndicator();
+            },
+          ),
+        ),
+      ),
+    );
+  }
 }
